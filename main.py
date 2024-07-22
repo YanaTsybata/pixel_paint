ddbb2684ad455c25fc808b34af820442f747f8e9
Narci <yanatsy002@gmail.com>
2024-07-22 22:29:57 +0200

    add possibility to paint whith pressed mouse

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ pygame.display.set_caption("Pixel artist")
 # current color
 current_color = black
 
+# mouse pressed or not
+mouse_pressed = False
+
 # cycle
 
 running = True
@@ -33,13 +36,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # localisation mouse
-            x,y = pygame.mouse.get_pos()
-            # editing coodrinates to net
-            grid_x = x // PIXEL_SIZE
-            grid_y = y // PIXEL_SIZE
-            # pixel painting
-            pygame.draw.rect(screen, current_color, (grid_x * PIXEL_SIZE, grid_y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+            mouse_pressed = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse_pressed =False
         elif event.type == pygame.KEYDOWN:
             #color change
             if event.key == pygame.K_r:
@@ -54,6 +53,16 @@ while running:
                 current_color = black
             if event.key == pygame.K_c:
                 screen.fill(white) # clear screen
+
+    # localisation mouse, if pressed paint
+    if mouse_pressed:
+        x, y = pygame.mouse.get_pos() # mouse position
+        # editing coodrinates to net
+        grid_x = x // PIXEL_SIZE
+        grid_y = y // PIXEL_SIZE
+        # pixel painting
+        pygame.draw.rect(screen, current_color,(grid_x * PIXEL_SIZE, grid_y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+
     # update screen
     pygame.display.flip()
 
